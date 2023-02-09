9256ca7571ba62ffc1f9e85c849a2c61d8e4f3ce
Adriano Rodrigues <o.adrian.rodrigues@gmail.com>
2023-02-08 21:52:45 -0300

    Adicionado while e if

--- Aplication/Program.java
+++ Aplication/Program.java
@@ -23,23 +23,44 @@ public class Program {
 
 		System.out.println();
 		System.out.println("Product data: " + product);
-		
+
 		System.out.println();
-		System.out.println("Enter the number of products to be added in stock: ");
-		int quantity = sc.nextInt();
-		product.addProducts(quantity);
-		
-		System.out.println();
-		System.out.println("Updated data: " + product);
-		
-		System.out.println();
-		System.out.println("Enter the number of products to be removed in stock: ");
-		quantity = sc.nextInt();
-		product.removeProducts(quantity);
-		
-		System.out.println("Update data: " + product);
+		System.out.println("Deseja Adicionar ou Remover itens(s/n)");
+		char resp = sc.next().charAt(0);
+		int quantity = 0;
+
+		while (resp == 's') {
+
+			System.out.println("Adicionar ou remover(a/r)");
+			resp = sc.next().charAt(0);
+
+			if (resp == 'a') {
+
+				System.out.println("Enter the number of products to be added in stock: ");
+				quantity = sc.nextInt();
+				product.addProducts(quantity);
+
+				System.out.println();
+				System.out.println("Updated data: " + product);
+				System.out.println("Deseja fazer mais alguma coisa s/n");
+				resp = sc.next().charAt(0);
+
+			} else if (resp == 'r') {
+				System.out.println();
+				System.out.println("Enter the number of products to be removed in stock: ");
+				quantity = sc.nextInt();
+				product.removeProducts(quantity);
+
+				System.out.println("Update data: " + product);
+				System.out.println("Deseja fazer mais alguma coisa s/n");
+				resp = sc.next().charAt(0);
+
+			} else {
+				System.out.println("Deseja fazer mais alguma coisa s/n");
+			}
+		}
+		System.out.println("Programa finalizado!");
 
 		sc.close();
 	}
-
 }
