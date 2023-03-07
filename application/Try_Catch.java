ed6ab04e8630a6b1c52fc668c84f5e1753bb28d3
Adriano Rodrigues <o.adrian.rodrigues@gmail.com>
2023-03-07 16:08:11 -0300

    Block Finally

--- application/Try_Catch.java
+++ application/Try_Catch.java
@@ -1,48 +1,29 @@
 package application;
 
-import java.util.InputMismatchException;
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 public class Try_Catch {
 
 	public static void main(String[] args) {
 
-		method1();
-		System.out.println("Ende of program");
+		File file = new File("C:\\temp\\in.txt");
+		Scanner sc = null;
 
-	}
-	
-	
-	public static void method1(){
-		System.out.println("***********METHOD1 START*************");
-		method2();
-		System.out.println("***********METHOD1 END*************");
-	}
-
-	public static void method2() {
-		
-		System.out.println("***********METHOD2 START*************");
-		Scanner sc = new Scanner(System.in);
-		
 		try {
-			
-			String[] vect = sc.nextLine().split(" ");
-			int position = sc.nextInt();
-			System.out.println(vect[position]);
-			
-		} catch (ArrayIndexOutOfBoundsException e) {
-			
-			System.out.println("Invalid Position");
-			e.printStackTrace();
-			
-		} catch (InputMismatchException e) {
-			
-			System.out.println("Input Erro");
-			
+			sc = new Scanner(file);
+			while (sc.hasNextLine()) {
+				System.out.println(sc.nextLine());
+			}
+
+		} catch (FileNotFoundException e) {
+			System.out.println("Error opening file: " + e.getMessage());
+		} finally {
+			if (sc != null) {
+				sc.close();
+			}
 		}
-		System.out.println("***********METHOD2 END*************");
-		sc.close();
 
 	}
-
 }
