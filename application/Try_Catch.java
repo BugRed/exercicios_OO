66f0b3968a4ff9cb901233e0fde27045d5c32ab8
Adriano Rodrigues <o.adrian.rodrigues@gmail.com>
2023-03-07 15:59:24 -0300

    tratamento de exceção e printStackTrace

--- application/Try_Catch.java
+++ application/Try_Catch.java
@@ -7,19 +7,42 @@ public class Try_Catch {
 
 	public static void main(String[] args) {
 
+		method1();
+		System.out.println("Ende of program");
+
+	}
+	
+	
+	public static void method1(){
+		System.out.println("***********METHOD1 START*************");
+		method2();
+		System.out.println("***********METHOD1 END*************");
+	}
+
+	public static void method2() {
+		
+		System.out.println("***********METHOD2 START*************");
 		Scanner sc = new Scanner(System.in);
+		
 		try {
-		String[] vect = sc.nextLine().split(" ");
-		int position = sc.nextInt();
-		System.out.println(vect[position]);
-		}
-		catch(ArrayIndexOutOfBoundsException e){
+			
+			String[] vect = sc.nextLine().split(" ");
+			int position = sc.nextInt();
+			System.out.println(vect[position]);
+			
+		} catch (ArrayIndexOutOfBoundsException e) {
+			
 			System.out.println("Invalid Position");
-		}
-		catch(InputMismatchException e) {
+			e.printStackTrace();
+			
+		} catch (InputMismatchException e) {
+			
 			System.out.println("Input Erro");
+			
 		}
+		System.out.println("***********METHOD2 END*************");
 		sc.close();
+
 	}
 
 }
