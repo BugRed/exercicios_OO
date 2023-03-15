f1221db076844de75b133106b54c5d748568d1bc
Adriano Rodrigues <o.adrian.rodrigues@gmail.com>
2023-03-14 21:19:07 -0300

    File manipulation and creation subdirectory

---
+++ application/File_Manipulation_ex.java
@@ -0,0 +1,39 @@
+package application;
+
+import java.io.File;
+import java.util.Scanner;
+
+public class File_Manipulation_ex {
+
+	public static void main(String[] args) {
+
+
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Enter de folder path: ");
+		String strPath = sc.nextLine();
+		
+		File path = new File(strPath);
+		
+		File[] folders = path.listFiles(File::isDirectory);
+		System.out.println("FOLDERS: ");
+		for (File folder : folders) {
+			System.out.println(folder);
+			
+		}
+		
+		File[] files = path.listFiles(File::isFile);
+		System.out.println("FILES: ");
+		for(File file : files) {
+			System.out.println(file);
+		}
+		
+		boolean success = new File(strPath + "\\subpasta").mkdir();
+		System.out.println("Directory created successfully: " + success);
+		
+		
+		sc.close();
+
+	}
+
+}
